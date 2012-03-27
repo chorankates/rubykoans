714ca3d61264209783af8f26b9f6bd39c0f8015c
Conor Horan-Kates <conor.code@gmail.com>
2012-03-27 08:23:00 -0700

    adding some exception handling to the method used in about_triangles\*.rb

--- triangle.rb
+++ triangle.rb
@@ -15,7 +15,13 @@
 #
 def triangle(a, b, c)
 
-   if a == b and a ==c
+   if a+b <= c or b+c <= a or a+c <= b # triangle inequality
+      raise(TriangleError, 'triangle inequality')
+   elsif a <= 0 or b <= 0 or c <= 0 # handles illegal lines 11,12
+      raise(TriangleError, 'side length of less than 0')
+   end
+
+  if a == b and a ==c
     return :equilateral
   elsif (a == b and a != c) or (a == c and a !=b) or (b == c and b != a)
     return :isosceles
