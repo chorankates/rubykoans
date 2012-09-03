209a53f0851c8faccc6d098feecc1ef760366c7c
Conor Horan-Kates <conor.code@gmail.com>
2012-09-03 13:03:41 -0700

    reading the ruby equivalent of the camel book was tough, but worth it

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -2,9 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 
 # Implement a DiceSet Class here:
 #
-# class DiceSet
-#   code ...
-# end
+class DiceSet
+  attr_reader :values
+  
+  def roll (count)
+    @values = [];
+    for i in 0...count
+      values.push(rand(max=5) + 1)
+    end
+    values
+  end
+  
+end
 
 class AboutDiceProject < EdgeCase::Koan
   def test_can_create_a_dice_set
